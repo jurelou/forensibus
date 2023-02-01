eb49fb61ed99d1bd425df0984c755ee0d5decb2a
jurelou <louis@jurczyk.fr>
2023-02-01 11:00:37 -0800

    .

--- processors/windows/artifacts/registry.go
+++ processors/windows/artifacts/registry.go
@@ -1,10 +1,15 @@
 package windows_artifacts
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
+	"time"
 
 	"www.velocidex.com/golang/regparser"
+	"www.velocidex.com/golang/regparser/appcompatcache"
 
 	"github.com/jurelou/forensibus/utils"
 	"github.com/jurelou/forensibus/utils/processors"
@@ -19,17 +24,72 @@ type RegistryProcessor struct {
 	processors.Default
 }
 
-func rec(nk *regparser.CM_KEY_NODE) {
-	fmt.Println("-- ", nk.LastWriteTime(), nk.Name())
+type RegistryEntry struct {
+	LastWriteTime string
+	Key           string
+	KeyName       string
+	Value         string
+	ValueName     string
+	// KeyName string
+}
+
+type AppCompatCacheEntry struct {
+	Time time.Time
+	Name string
+	Id   int
+}
+
+func dumpAppCompatCache(nk *regparser.CM_KEY_NODE, out writer.OutputWriter) {
+	out.SetDefaultSourceType("forensibus_appcompatcache")
+
+	for _, value := range nk.Values() {
+		if value.ValueName() != "AppCompatCache" {
+			continue
+		}
+		for idx, entry := range appcompatcache.ParseValueData(value.ValueData().Data) {
+			fmt.Printf("%d: %v  %v\n", idx, entry.Time, entry.Name)
+			entry := AppCompatCacheEntry{
+				Time: entry.Time,
+				Name: entry.Name,
+				Id:   idx,
+			}
+			jsonEntry, _ := json.Marshal(entry)
+			e := writer.NewEvent(string(jsonEntry))
+			out.WriteEvent(e)
+		}
+	}
+}
+
+func rec(nk *regparser.CM_KEY_NODE, path string, out writer.OutputWriter) {
+	keyName := nk.Name()
+	lastWrite := nk.LastWriteTime().GoString()
+	path = path + "/" + keyName
+
 	for _, v := range nk.Values() {
-		fmt.Println("   >", v.Name(), v.ValueName(), v.ValueData())
+		valueData := v.ValueData()
+		entry := RegistryEntry{
+			LastWriteTime: lastWrite,
+			Key:           path,
+			KeyName:       keyName,
+			ValueName:     v.ValueName(),
+		}
+
+		if valueData.String != "" {
+			entry.Value = valueData.String
+		} else if len(valueData.MultiSz) > 0 {
+			entry.Value = strings.Join(valueData.MultiSz, ",")
+		} else if valueData.Uint64 != 0 {
+			entry.Value = strconv.FormatUint(valueData.Uint64, 10)
+		} else {
+			entry.Value = fmt.Sprintf("<omit %s>", v.TypeString())
+		}
+		jsonEntry, _ := json.Marshal(entry)
+		e := writer.NewEvent(string(jsonEntry))
+		out.WriteEvent(e)
 	}
 
 	for _, subkey := range nk.Subkeys() {
-		// fmt.Println("<<<<<<<<<<<", subkey.Name())
-		fmt.Println("-- ", subkey.LastWriteTime(), subkey.Name())
-		return
-		rec(subkey)
+		rec(subkey, path, out)
 	}
 }
 
@@ -41,29 +101,25 @@ func (RegistryProcessor) Run(in string, out writer.OutputWriter) processors.PErr
 		errors.Add(err)
 		return errors
 	}
-
-	// fmt.Println(">>>>>>>>>>>>", in)
 	registry, err := regparser.NewRegistry(fd)
-	// accKey := registry.OpenKey(appcompatcache_path)
-	// if accKey != nil {
-	// 	fmt.Println("APP COMPAT CACHE HERE", in)
-	// 	return errors
-	// }
 	if err != nil {
 		errors.Add(err)
 		return errors
 	}
+
+	accKey := registry.OpenKey(appcompatcache_path)
+	if accKey != nil {
+		dumpAppCompatCache(accKey, out)
+	}
+
 	rootCell := registry.Profile.HCELL(registry.Reader, 0x1000+int64(registry.BaseBlock.RootCell()))
 	nk := rootCell.KeyNode()
 	if nk == nil {
 		errors.Add(fmt.Errorf("Could not find root cell from %s", in))
 		return errors
 	}
-
-	fmt.Println("@@@@@@@@@@@", in)
-	rec(nk)
-
-	// time.Sleep(1 * time.Second)
+	out.SetDefaultSourceType("forensibus_registry")
+	rec(nk, "", out)
 
 	return errors
 }
